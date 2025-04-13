a20ab9f3a8d61d25c18d7bb53f8c606f1af5b48b
cjd <carljduff@gmail.com>
2025-04-12 20:12:52 -0400

    added swapped and comparsion counts to selection_sort and added quick_sort code

--- main.py
+++ main.py
@@ -19,20 +19,54 @@ def bubble_sort(my_list):
         if not swapped:
             break
 
-    return swapped_count, compared_count
+    return my_list, swapped_count, compared_count
 
 def selection_sort(my_list):
     n = len(my_list)
+    swapped_count = 0
+    compared_count = 0
 
     for i in range(n):
         min_idx = i
+
         for j in range(i+1, n):
+            compared_count += 1
             if my_list[min_idx] > my_list[j]:
                 min_idx = j
 
-        my_list[i], my_list[min_idx] = my_list[min_idx], my_list[i]
+        if min_idx != i:
+            my_list[i], my_list[min_idx] = my_list[min_idx], my_list[i]
+            swapped_count += 1
 
+    return my_list, swapped_count, compared_count
 
+quick_sort_count = 0
+
+def quick_sort(my_list):
+    global quick_sort_count
+
+    less = []
+    equal = []
+    greater = []
+
+    if len(my_list) > 1:
+        quick_sort_count += 1
+        pivot = my_list[0]
+
+        for x in my_list:
+            if x < pivot:
+                less.append(x)
+                quick_sort_count += 1
+            if x == pivot:
+                equal.append(x)
+                quick_sort_count += 1
+            if x > pivot:
+                greater.append(x)
+                quick_sort_count += 1
+
+        return quick_sort(less) + equal + quick_sort(greater)
+    else:
+        return my_list
 
 if __name__ == '__main__':
     main()
